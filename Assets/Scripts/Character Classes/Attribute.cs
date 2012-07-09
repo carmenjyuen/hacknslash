ca58d805f713b7b34bc464e0b9aaacc16b51e57b
Carmen Yuen <carmenjyuen@gmail.com>
2012-07-08 20:24:58 -0700

    Added comments
    Tutorial 39 Done

--- Assets/Scripts/Character Classes/Attribute.cs
+++ Assets/Scripts/Character Classes/Attribute.cs
@@ -1,8 +1,18 @@
+/// <summary>
+/// Attribute.cs 
+/// Carmen Yuen
+/// July 8, 2012
+/// 
+/// This is the class for all of the character attributes in game.
+/// </summary>
 public class Attribute : BaseStat {
-	new public const int STARTING_EXP_COST = 50;
+	new public const int STARTING_EXP_COST = 50;	//this is the starting cost for all of the attributes
 	
-	private string _name;
+	private string _name;							//this is the name of the attribute
 	
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Attribute"/> class.
+	/// </summary>
 	public Attribute(){
 		_name = "";
 		ExpToLevel = STARTING_EXP_COST;
@@ -10,13 +20,24 @@ public class Attribute : BaseStat {
 		
 	}
 	
+	/// <summary>
+	/// Gets or sets the name.
+	/// </summary>
+	/// <value>
+	/// The name.
+	/// </value>
 	public string Name {
 		get{ return _name; }
 		set{ _name = value; }
 	}
 	
 }
+
 // Adding a note to revise Attributes
+
+/// <summary>
+/// List of the Attributes that will be in game for characters.
+/// </summary>
 public enum AttributeName {
 	Strength,
 	Stamina,
