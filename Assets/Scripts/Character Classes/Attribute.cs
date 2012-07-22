3e0b8e6d7b5e0571d581cd4875fcf0d2cee29a3a
Carmen Yuen <carmenjyuen@gmail.com>
2012-07-21 17:37:11 -0700

    Buff Item Class added
    Tutorial 62 Done

--- Assets/Scripts/Character Classes/Attribute.cs
+++ Assets/Scripts/Character Classes/Attribute.cs
@@ -8,29 +8,17 @@
 public class Attribute : BaseStat {
 	new public const int STARTING_EXP_COST = 50;	//this is the starting cost for all of the attributes
 	
-	private string _name;							//this is the name of the attribute
 	
 	/// <summary>
 	/// Initializes a new instance of the <see cref="Attribute"/> class.
 	/// </summary>
 	public Attribute(){
 		UnityEngine.Debug.Log("Attribute Created");
-		_name = "";
 		ExpToLevel = STARTING_EXP_COST;
 		LevelModifier = 1.05f;
 		
 	}
 	
-	/// <summary>
-	/// Gets or sets the name.
-	/// </summary>
-	/// <value>
-	/// The name.
-	/// </value>
-	public string Name {
-		get{ return _name; }
-		set{ _name = value; }
-	}
 	
 }
 
