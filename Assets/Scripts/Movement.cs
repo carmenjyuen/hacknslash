8e6749e0d9150e2a3f6c9ab38fe77488f384edcf
Carmen Yuen <carmenjyuen@gmail.com>
2012-08-10 18:03:52 -0700

    Moving forward/backward
    Tutorial 93 Done

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(CharacterController))]
 public class Movement : MonoBehaviour {
+	public float moveSpeed = 5;
 	public float rotateSpeed = 250;
 	private Transform _myTransform;
+	private CharacterController _controller;
 	
 	public void Awake() {
 		_myTransform = transform;
+		_controller = GetComponent<CharacterController>();
 	}
 	
 	//Use this for initialization
@@ -18,6 +22,11 @@ public class Movement : MonoBehaviour {
 	void Update() {
 		if(Mathf.Abs(Input.GetAxis("Rotate Player")) > 0){
 			_myTransform.Rotate(0, Input.GetAxis("Rotate Player") * Time.deltaTime * rotateSpeed, 0);	
+		}
+		
+		if(Mathf.Abs(Input.GetAxis("Move Forward")) > 0){
+//			Debug.Log("Forward" + Input.GetAxis("Move Forward"));
+			_controller.SimpleMove(_myTransform.TransformDirection(Vector3.forward)* Input.GetAxis("Move Forward") * moveSpeed);
 		}
 	}
 		
