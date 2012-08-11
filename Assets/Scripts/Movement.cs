c6425e93c3569992831e455b271913759cc15f6b
Carmen Yuen <carmenjyuen@gmail.com>
2012-08-11 15:26:01 -0700

    Added Chub and More improvements on Movement Script
    Tutorial 94-95 Done
    
    Made Chubby from scratch and animated it in Blender. Learned to hook it
    up to Unity.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 [RequireComponent (typeof(CharacterController))]
 public class Movement : MonoBehaviour {
-	public float moveSpeed = 5;
+	public float moveSpeed = 3;
+	public float runMultiplier = 2;
+	public float strafeSpeed = 2.5f;
 	public float rotateSpeed = 250;
 	private Transform _myTransform;
 	private CharacterController _controller;
@@ -15,21 +17,48 @@ public class Movement : MonoBehaviour {
 	
 	//Use this for initialization
 	void Start() {
-		
+		animation.wrapMode = WrapMode.Loop;
 	}
 	
 	//Update is called once per frame 
 	void Update() {
-		if(Mathf.Abs(Input.GetAxis("Rotate Player")) > 0){
-			_myTransform.Rotate(0, Input.GetAxis("Rotate Player") * Time.deltaTime * rotateSpeed, 0);	
-		}
-		
-		if(Mathf.Abs(Input.GetAxis("Move Forward")) > 0){
-//			Debug.Log("Forward" + Input.GetAxis("Move Forward"));
-			_controller.SimpleMove(_myTransform.TransformDirection(Vector3.forward)* Input.GetAxis("Move Forward") * moveSpeed);
-		}
+		Turn();
+		Walk();
+		Strafe();
+				
+
 	}
 		
+	private void Turn() {
+		if(Mathf.Abs(Input.GetAxis("Rotate Player")) > 0){
+			_myTransform.Rotate(0, Input.GetAxis("Rotate Player") * Time.deltaTime * rotateSpeed, 0);	
+		}	
+	}
 	
+	private void Walk(){
+		if(Mathf.Abs(Input.GetAxis("Move Forward")) > 0){
+//			Debug.Log("Forward" + Input.GetAxis("Move Forward"));
+			if(Input.GetButton("Run")) {
+				animation.CrossFade("run");
+				_controller.SimpleMove(_myTransform.TransformDirection(Vector3.forward)* Input.GetAxis("Move Forward") * moveSpeed * runMultiplier);
+				
+			}
+			else { 
+				animation.CrossFade("walk");
+				_controller.SimpleMove(_myTransform.TransformDirection(Vector3.forward)* Input.GetAxis("Move Forward") * moveSpeed);
+			}
+			
+		}
+		else {
+			animation.CrossFade("idle");
+		}
+	}
+	
+	private void Strafe(){
+		if(Mathf.Abs(Input.GetAxis("Strafe")) > 0){
+//			Debug.Log("Strafe" + Input.GetAxis("Strafe"));
+			_controller.SimpleMove(_myTransform.TransformDirection(Vector3.right)* Input.GetAxis("Strafe") * strafeSpeed);
+		}
+	}
 	
 }
