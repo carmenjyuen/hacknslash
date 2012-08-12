014cab6029b6702287a58aa3f129f07cadf926fc
Carmen Yuen <carmenjyuen@gmail.com>
2012-08-12 10:11:44 -0700

    Fixed problems after merging to master
    Apparently Unity and Github on windows don't like to mix.

--- Assets/Scripts/Character Classes/PlayerCharacter.cs
+++ Assets/Scripts/Character Classes/PlayerCharacter.cs
@@ -1,6 +1,6 @@
 public class PlayerCharacter : BaseCharacter {
 	void Update() {
-		Messenger<int, int>.Broadcast("player health update", 80, 100); 
+		Messenger<int, int>.Broadcast("player health update", 80, 100, MessengerMode.DONT_REQUIRE_LISTENER); 
 			
 	}
 
