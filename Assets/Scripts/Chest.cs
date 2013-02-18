ee052ee22d81cc31d6b40cbd968273ec9d16f8b1
Carmen <carmenjyuen@gmail.com>
2013-02-17 21:03:09 -0800

    Added more effects to chest
    Tutorial 113 Done

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(BoxCollider))]
+[RequireComponent (typeof(AudioSource))]
 public class Chest : MonoBehaviour {
 	
 	public enum State {
@@ -12,11 +14,24 @@ public class Chest : MonoBehaviour {
 	public AudioClip openSound;
 	public AudioClip closeSound;
 	
+	public GameObject particleEffect;
+	
+	public GameObject[] parts;
+	private Color[] _defaultColors;
+	
 	public State state;
 	
 	// Use this for initialization
 	void Start () {
 		state = Chest.State.close;
+		
+		particleEffect.active = false;
+		
+		_defaultColors = new Color[parts.Length];
+		
+		if(parts.Length > 0)
+			for(int cnt = 0; cnt < _defaultColors.Length; cnt++)
+				_defaultColors[cnt] = parts[cnt].renderer.material.GetColor("_Color");
 	}
 	
 	// Update is called once per frame
@@ -27,10 +42,12 @@ public class Chest : MonoBehaviour {
 	
 	public void OnMouseEnter() {
 		Debug.Log("Enter");	
+		Highlight(true);
 	}
 	
 	public void OnMouseExit() {
 		Debug.Log("Exit");	
+		Highlight(false);
 	}
 	public void OnMouseUp() {
 		Debug.Log("Up");	
@@ -52,6 +69,7 @@ public class Chest : MonoBehaviour {
 	
 	private IEnumerator Open() {
 		animation.Play("open");
+		particleEffect.active = true;
 		audio.PlayOneShot(openSound);
 		yield return new WaitForSeconds(animation["open"].length);
 		state = Chest.State.open;
@@ -59,9 +77,23 @@ public class Chest : MonoBehaviour {
 	
 	private IEnumerator Close() {
 		animation.Play("close");
+		particleEffect.active = false;
 		audio.PlayOneShot(closeSound);	
 		yield return new WaitForSeconds(animation["close"].length);
 		state = Chest.State.close;
+	}
+	
+	private void Highlight(bool glow) {
+		if(glow) {
+			if(parts.Length > 0)
+				for(int cnt = 0; cnt < _defaultColors.Length; cnt++)
+					parts[cnt].renderer.material.SetColor("_Color",Color.yellow);			
+		}
+		else { 
+			if(parts.Length > 0)
+			for(int cnt = 0; cnt < _defaultColors.Length; cnt++)
+				parts[cnt].renderer.material.SetColor("_Color", _defaultColors[cnt]);
+		}
 	}
 	
 }
